3a037a8700b2d8283ed1c64b4253d9f2da1e65a7
Bruno <69259703+bpcavalcante@users.noreply.github.com>
2020-11-04 16:34:28 -0300

    Criando teste para os usuários

--- spring-course/src/test/java/com/springcourse/SpringCourseApplicationTests.java
+++ spring-course/src/test/java/com/springcourse/SpringCourseApplicationTests.java
@@ -1,13 +1,16 @@
 package com.springcourse;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringCourseApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
